6fae79bcd2ef30ced655de10fa44b9266f69b425
ToudouSeiya <stwinavenger@gmail.com>
2026-09-24 10:15:22 -0400

    moonlight

--- weekly4.js
+++ weekly4.js
@@ -275,14 +275,14 @@ function render()
     //light location
     gl.uniform3f(
         uniforms.lightDirection,
-        -1,
-        0.5,
+        1,
+        1,
         0
     );
     //color of the light
-    gl.uniform3f(uniforms.lightColor, 1, 0.6, 0);
+    gl.uniform3f(uniforms.lightColor, 0.2, 0.3, 1);
     //ambient strength
-    gl.uniform1f(uniforms.ambient, 0.4);
+    gl.uniform1f(uniforms.ambient, 0.1);
     draw(ground, identity(), [0.35, 0.38, 0.42]);
     draw(cube, transform(-2.5, 1, 0, 1, 0), [0.9, 0.25, 0.2]);
     draw(cube, transform(2.5, 1, 0, 1, 0), [0.2, 0.45, 0.95]);
